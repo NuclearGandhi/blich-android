97a125f8115651364540a6c67a5e6e0e5438d8c1
dodobentov@gmail.com <dodobentov@gmail.com>
2016-07-31 10:25:11 +0300

    Add navigation drawer

--- app/src/main/java/com/blackcracks/blich/activity/MainActivity.java
+++ app/src/main/java/com/blackcracks/blich/activity/MainActivity.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 import com.blackcracks.blich.R;
+import com.blackcracks.blich.adapter.DrawerListAdapter;
 import com.blackcracks.blich.fragment.ScheduleFragment;
 
 import java.util.Locale;
@@ -33,16 +35,21 @@ public class MainActivity extends AppCompatActivity {
         getBaseContext().getResources().updateConfiguration(config,
                 getBaseContext().getResources().getDisplayMetrics());
 
+        ListView drawerList = (ListView) findViewById(R.id.drawer_list);
+        String[] titles = getResources().getStringArray(R.array.drawer_titles);
+        int[] icons = new int[]{R.drawable.ic_schedule_grey_24dp};
+        drawerList.setAdapter(new DrawerListAdapter(this, titles, icons));
+
         if (savedInstanceState != null) {
             mScheduleFragment = (ScheduleFragment) getSupportFragmentManager().
                     getFragment(savedInstanceState, SCHEDULE_FRAGMENT_TAG);
         } else {
             mScheduleFragment = new ScheduleFragment();
         }
+
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment, mScheduleFragment, SCHEDULE_FRAGMENT_TAG)
                 .commit();
-
     }
 
     @Override
