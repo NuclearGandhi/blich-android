35820581f5ba8281fbb28b7120687e0e3b1ae3d3
dodobentov@gmail.com <dodobentov@gmail.com>
2017-01-31 15:59:41 +0200

    Add the ExamsFragment

---
+++ app/src/main/java/com/blackcracks/blich/fragment/ExamsFragment.java
@@ -0,0 +1,23 @@
+package com.blackcracks.blich.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.blackcracks.blich.R;
+
+public class ExamsFragment extends Fragment {
+
+    public ExamsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_exams, container, false);
+    }
+
+}
