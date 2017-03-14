8203905adbbdd082f403553ebcc9da0b5bde0cb5
dodobentov@gmail.com <dodobentov@gmail.com>
2017-03-14 18:27:40 +0200

    Change the database's onUpgrade(), to a more proper one

--- app/src/main/java/com/blackcracks/blich/data/BlichDatabaseHelper.java
+++ app/src/main/java/com/blackcracks/blich/data/BlichDatabaseHelper.java
@@ -4,10 +4,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.blackcracks.blich.data.BlichContract.ClassEntry;
-import com.blackcracks.blich.data.BlichContract.ExamsEntry;
-import com.blackcracks.blich.data.BlichContract.ScheduleEntry;
-
+import com.blackcracks.blich.data.BlichContract.*;
 public class BlichDatabaseHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 5;
@@ -53,9 +50,18 @@ public class BlichDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + ScheduleEntry.TABLE_NAME);
-        db.execSQL("DROP TABLE IF EXISTS " + ExamsEntry.TABLE_NAME);
-        db.execSQL("DROP TABLE IF EXISTS " + ClassEntry.TABLE_NAME);
+        if (oldVersion < 5) {
+
+            final String SQL_CREATE_EXAMS_TABLE = "CREATE TABLE " + ExamsEntry.TABLE_NAME + " (" +
+                    ExamsEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    ExamsEntry.COL_SUBJECT + " TEXT NOT NULL," +
+                    ExamsEntry.COL_DATE + " TEXT NOT NULL, " +
+                    ExamsEntry.COL_TEACHER + " TEXT NOT NULL, " +
+                    "UNIQUE (" + ExamsEntry.COL_SUBJECT + ", " + ExamsEntry.COL_DATE + ", " +
+                    ExamsEntry.COL_TEACHER + ") ON CONFLICT REPLACE);";
+
+            db.execSQL(SQL_CREATE_EXAMS_TABLE);
+        }
 
         onCreate(db);
     }
