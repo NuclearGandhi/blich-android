30e7e97de551e609f2fd80227d384a6a4cf92be5
dodobentov@gmail.com <dodobentov@gmail.com>
2017-04-25 17:20:11 +0300

    Add a support email

--- app/src/main/java/com/blackcracks/blich/activity/AboutActivity.java
+++ app/src/main/java/com/blackcracks/blich/activity/AboutActivity.java
@@ -1,15 +1,30 @@
 package com.blackcracks.blich.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.blackcracks.blich.R;
 
 public class AboutActivity extends AppCompatActivity {
 
+    private static final String[] DEV_EMAIL = {"nukegandhi@gmail.com"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    public void onEmailClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, DEV_EMAIL);
+        intent.setData(Uri.parse("mailto:"));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
