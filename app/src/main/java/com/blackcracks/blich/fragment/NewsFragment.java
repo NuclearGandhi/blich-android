e676627bf5560859661d097327be90e560b264de
dodobentov@gmail.com <dodobentov@gmail.com>
2017-04-30 21:07:34 +0300

    Add required methods from BlichBaseFragment

--- app/src/main/java/com/blackcracks/blich/fragment/NewsFragment.java
+++ app/src/main/java/com/blackcracks/blich/fragment/NewsFragment.java
@@ -2,20 +2,33 @@ package com.blackcracks.blich.fragment;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.blackcracks.blich.R;
 
-public class NewsFragment extends Fragment {
+public class NewsFragment extends BlichBaseFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    protected int getFragmentLayout() {
+        return R.layout.fragment_news;
+    }
+
+    @Override
+    protected int getFragmentTitle() {
+        return R.string.drawer_news_title;
+    }
+
+    @Override
+    protected int getMenuResource() {
+        return R.menu.fragment_news;
     }
 
 }
