682ea3343e423ef47160ec318766f55107a0f042
dodobentov@gmail.com <dodobentov@gmail.com>
2017-05-16 20:53:04 +0300

    Remove duplicate variables

--- app/src/main/java/com/blackcracks/blich/data/BlichDatabaseHelper.java
+++ app/src/main/java/com/blackcracks/blich/data/BlichDatabaseHelper.java
@@ -11,50 +11,53 @@ public class BlichDatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "blich.db";
 
+    private final String SQL_CREATE_SCHEDULE_TABLE = "CREATE TABLE " + ScheduleEntry.TABLE_NAME + " (" +
+            ScheduleEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            ScheduleEntry.COL_CLASS_SETTINGS + " INTEGER NOT NULL, " +
+            ScheduleEntry.COL_DAY + " INTEGER NOT NULL, " +
+            ScheduleEntry.COL_HOUR + " INTEGER NOT NULL, " +
+            ScheduleEntry.COL_LESSON + " INTEGER NOT NULL, " +
+            ScheduleEntry.COL_LESSON_COUNT + " INTEGER, " +
+            ScheduleEntry.COL_SUBJECT + " TEXT NOT NULL, " +
+            ScheduleEntry.COL_CLASSROOM + " TEXT, " +
+            ScheduleEntry.COL_TEACHER + " TEXT, " +
+            ScheduleEntry.COL_LESSON_TYPE + " TEXT NOT NULL, " +
+            "UNIQUE (" + ScheduleEntry.COL_DAY + ", " + ScheduleEntry.COL_HOUR + ", " + ScheduleEntry.COL_LESSON +
+            ") ON CONFLICT REPLACE);";
+
+    final String SQL_DROP_SCHEDULE_TABLE = "DROP TABLE " + ScheduleEntry.TABLE_NAME + ";";
+
+    private final String SQL_CREATE_EXAMS_TABLE = "CREATE TABLE " + ExamsEntry.TABLE_NAME + " (" +
+            ExamsEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            ExamsEntry.COL_SUBJECT + " TEXT NOT NULL," +
+            ExamsEntry.COL_DATE + " TEXT NOT NULL, " +
+            ExamsEntry.COL_TEACHER + " TEXT NOT NULL, " +
+            "UNIQUE (" + ExamsEntry.COL_SUBJECT + ", " + ExamsEntry.COL_DATE + ", " +
+            ExamsEntry.COL_TEACHER + ") ON CONFLICT REPLACE);";
+
+    private final String SQL_CREATE_NEWS_TABLE = "CREATE TABLE " + NewsEntry.TABLE_NAME + " (" +
+            NewsEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            NewsEntry.COL_CATEGORY + " INTEGER NOT NULL, " +
+            NewsEntry.COL_TITLE + " TEXT NOT NULL, " +
+            NewsEntry.COL_BODY + " TEXT NOT NULL, " +
+            NewsEntry.COL_AUTHOR + " TEXT NOT NULL, " +
+            NewsEntry.COL_DATE + " TEXT NOT NULL, " +
+            "UNIQUE (" + NewsEntry.COL_TITLE + ", " + ExamsEntry.COL_DATE + ", " +
+            NewsEntry.COL_AUTHOR + ") ON CONFLICT REPLACE);";
+
+    private final String SQL_CREATE_CLASS_TABLE = "CREATE TABLE " + ClassEntry.TABLE_NAME + " (" +
+            ClassEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            ClassEntry.COL_CLASS_INDEX + " INTEGER NOT NULL UNIQUE ON CONFLICT REPLACE, " +
+            ClassEntry.COL_GRADE + " TEXT NOT NULL, " +
+            ClassEntry.COL_GRADE_INDEX + " INTEGER);";
+
+
     public BlichDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        final String SQL_CREATE_SCHEDULE_TABLE = "CREATE TABLE " + ScheduleEntry.TABLE_NAME + " (" +
-                ScheduleEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                ScheduleEntry.COL_CLASS_SETTINGS + " INTEGER NOT NULL, " +
-                ScheduleEntry.COL_DAY + " INTEGER NOT NULL, " +
-                ScheduleEntry.COL_HOUR + " INTEGER NOT NULL, " +
-                ScheduleEntry.COL_LESSON + " INTEGER NOT NULL, " +
-                ScheduleEntry.COL_LESSON_COUNT + " INTEGER, " +
-                ScheduleEntry.COL_SUBJECT + " TEXT NOT NULL, " +
-                ScheduleEntry.COL_CLASSROOM + " TEXT, " +
-                ScheduleEntry.COL_TEACHER + " TEXT, " +
-                ScheduleEntry.COL_LESSON_TYPE + " TEXT NOT NULL, " +
-                "UNIQUE (" + ScheduleEntry.COL_DAY + ", " + ScheduleEntry.COL_HOUR + ", " + ScheduleEntry.COL_LESSON +
-                ") ON CONFLICT REPLACE);";
-
-        final String SQL_CREATE_EXAMS_TABLE = "CREATE TABLE " + ExamsEntry.TABLE_NAME + " (" +
-                ExamsEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                ExamsEntry.COL_SUBJECT + " TEXT NOT NULL," +
-                ExamsEntry.COL_DATE + " TEXT NOT NULL, " +
-                ExamsEntry.COL_TEACHER + " TEXT NOT NULL, " +
-                "UNIQUE (" + ExamsEntry.COL_SUBJECT + ", " + ExamsEntry.COL_DATE + ", " +
-                ExamsEntry.COL_TEACHER + ") ON CONFLICT REPLACE);";
-
-        final String SQL_CREATE_NEWS_TABLE = "CREATE TABLE " + NewsEntry.TABLE_NAME + " (" +
-                NewsEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                NewsEntry.COL_CATEGORY + " INTEGER NOT NULL, " +
-                NewsEntry.COL_TITLE + " TEXT NOT NULL, " +
-                NewsEntry.COL_BODY + " TEXT NOT NULL, " +
-                NewsEntry.COL_AUTHOR + " TEXT NOT NULL, " +
-                NewsEntry.COL_DATE + " TEXT NOT NULL, " +
-                "UNIQUE (" + NewsEntry.COL_TITLE + ", " + ExamsEntry.COL_DATE + ", " +
-                NewsEntry.COL_AUTHOR + ") ON CONFLICT REPLACE);";
-
-        final String SQL_CREATE_CLASS_TABLE = "CREATE TABLE " + ClassEntry.TABLE_NAME + " (" +
-                ClassEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                ClassEntry.COL_CLASS_INDEX + " INTEGER NOT NULL UNIQUE ON CONFLICT REPLACE, " +
-                ClassEntry.COL_GRADE + " TEXT NOT NULL, " +
-                ClassEntry.COL_GRADE_INDEX + " INTEGER);";
-
         db.execSQL(SQL_CREATE_SCHEDULE_TABLE);
         db.execSQL(SQL_CREATE_EXAMS_TABLE);
         db.execSQL(SQL_CREATE_NEWS_TABLE);
@@ -64,31 +67,9 @@ public class BlichDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         if (oldVersion < 5) {
-            final String SQL_CREATE_EXAMS_TABLE = "CREATE TABLE " + ExamsEntry.TABLE_NAME + " (" +
-                    ExamsEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    ExamsEntry.COL_SUBJECT + " TEXT NOT NULL," +
-                    ExamsEntry.COL_DATE + " TEXT NOT NULL, " +
-                    ExamsEntry.COL_TEACHER + " TEXT NOT NULL, " +
-                    "UNIQUE (" + ExamsEntry.COL_SUBJECT + ", " + ExamsEntry.COL_DATE + ", " +
-                    ExamsEntry.COL_TEACHER + ") ON CONFLICT REPLACE);";
-
             db.execSQL(SQL_CREATE_EXAMS_TABLE);
         }
         if (oldVersion < 6) {
-            final String SQL_DROP_SCHEDULE_TABLE = "DROP TABLE " + ScheduleEntry.TABLE_NAME + ";";
-            final String SQL_CREATE_SCHEDULE_TABLE = "CREATE TABLE " + ScheduleEntry.TABLE_NAME + " (" +
-                    ScheduleEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    ScheduleEntry.COL_CLASS_SETTINGS + " INTEGER NOT NULL, " +
-                    ScheduleEntry.COL_DAY + " INTEGER NOT NULL, " +
-                    ScheduleEntry.COL_HOUR + " INTEGER NOT NULL, " +
-                    ScheduleEntry.COL_LESSON + " INTEGER NOT NULL, " +
-                    ScheduleEntry.COL_SUBJECT + " TEXT NOT NULL, " +
-                    ScheduleEntry.COL_CLASSROOM + " TEXT, " +
-                    ScheduleEntry.COL_TEACHER + " TEXT, " +
-                    ScheduleEntry.COL_LESSON_TYPE + " TEXT NOT NULL, " +
-                    "UNIQUE (" + ScheduleEntry.COL_DAY + ", " + ScheduleEntry.COL_HOUR + ", " + ScheduleEntry.COL_LESSON +
-                    ") ON CONFLICT REPLACE);";
-
             db.execSQL(SQL_DROP_SCHEDULE_TABLE);
             db.execSQL(SQL_CREATE_SCHEDULE_TABLE);
         }
@@ -99,16 +80,6 @@ public class BlichDatabaseHelper extends SQLiteOpenHelper {
             db.execSQL(SQL_ALTER_SCHEDULE_TABLE);
         }
         if (oldVersion < 8) {
-            final String SQL_CREATE_NEWS_TABLE = "CREATE TABLE " + NewsEntry.TABLE_NAME + " (" +
-                    NewsEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    NewsEntry.COL_CATEGORY + " INTEGER NOT NULL, " +
-                    NewsEntry.COL_TITLE + " TEXT NOT NULL, " +
-                    NewsEntry.COL_BODY + " TEXT NOT NULL, " +
-                    NewsEntry.COL_AUTHOR + " TEXT NOT NULL, " +
-                    NewsEntry.COL_DATE + " TEXT NOT NULL, " +
-                    "UNIQUE (" + NewsEntry.COL_TITLE + ", " + ExamsEntry.COL_DATE + ", " +
-                    NewsEntry.COL_AUTHOR + ") ON CONFLICT REPLACE);";
-
             db.execSQL(SQL_CREATE_NEWS_TABLE);
         }
     }
