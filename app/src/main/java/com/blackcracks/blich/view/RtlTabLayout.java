9a9af4ef07a2fda9ef79763a6ffb663177e8c657
dodobentov@gmail.com <dodobentov@gmail.com>
2017-07-21 11:57:31 +0300

    Refactor RtlTabLayout to the 'view' package from the 'widget' package

--- app/src/main/java/com/blackcracks/blich/view/RtlTabLayout.java
+++ app/src/main/java/com/blackcracks/blich/view/RtlTabLayout.java
@@ -1,4 +1,11 @@
-package com.blackcracks.blich.widget;
+/*
+ * Copyright (C) Ido Fang Bentov - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Ido Fang Bentov <dodobentov@gmail.com>, 2017
+ */
+
+package com.blackcracks.blich.view;
 
 import android.content.Context;
 import android.support.design.widget.TabLayout;
