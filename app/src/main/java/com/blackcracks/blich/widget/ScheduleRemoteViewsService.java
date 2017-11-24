77062dd1139101a0640908e69434b9b63537037b
dodobentov@gmail.com <dodobentov@gmail.com>
2017-11-24 10:04:27 +0200

    Remove schedule and lesson from the widget RemoteViews

--- app/src/main/java/com/blackcracks/blich/widget/ScheduleRemoteViewsService.java
+++ app/src/main/java/com/blackcracks/blich/widget/ScheduleRemoteViewsService.java
@@ -4,8 +4,9 @@ import android.content.Intent;
 import android.widget.RemoteViewsService;
 
 public class ScheduleRemoteViewsService extends RemoteViewsService {
+
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
-        return new ScheduleRemoteViewsFactory(getBaseContext(), intent);
+        return null;
     }
 }
