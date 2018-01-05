e5ce9798ab3ba71562426342dd245617b3ed84b8
dodobentov@gmail.com <dodobentov@gmail.com>
2018-01-05 23:18:38 +0200

    Add copyright

--- app/src/main/java/com/blackcracks/blich/data/FetchNewsService.java
+++ app/src/main/java/com/blackcracks/blich/data/FetchNewsService.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) Ido Fang Bentov - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Ido Fang Bentov <dodobentov@gmail.com>, 2017
+ */
+
 package com.blackcracks.blich.data;
 
 import android.app.IntentService;
@@ -80,7 +87,8 @@ public class FetchNewsService extends IntentService {
         Utilities.News.setIsFetchingForCategory(getBaseContext(), category, false);
     }
 
-    private @BlichSyncTask.FetchStatus int fetchNews(@NewsCategory int category) {
+    private @BlichSyncTask.FetchStatus
+    int fetchNews(@NewsCategory int category) {
         BufferedReader reader = null;
         String html = null;
 
