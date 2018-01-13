e948e1f487596eab04aef0211e4fdfaae3440cb1
dodobentov@gmail.com <dodobentov@gmail.com>
2018-01-13 09:24:22 +0200

    Add Realm support for the views factory

--- app/src/main/java/com/blackcracks/blich/widget/ScheduleRemoteViewsFactory.java
+++ app/src/main/java/com/blackcracks/blich/widget/ScheduleRemoteViewsFactory.java
@@ -1,28 +1,69 @@
 package com.blackcracks.blich.widget;
 
+import android.content.Context;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
+import com.blackcracks.blich.data.Lesson;
+import com.blackcracks.blich.util.Constants;
+import com.blackcracks.blich.util.Utilities;
+
+import java.util.List;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+import io.realm.Sort;
+
 public class ScheduleRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+
+    private Context mContext;
+    private Realm mRealm;
+
+    private int mDay;
+    private RealmWidgetHelper mRealmHelper;
+
+    public ScheduleRemoteViewsFactory(Context context, int day) {
+        mContext = context;
+        mDay = day;
+        mRealmHelper = new RealmWidgetHelper(null);
+    }
 
     @Override
     public void onCreate() {
-
+        mRealm = Realm.getDefaultInstance();
     }
 
     @Override
     public void onDataSetChanged() {
 
+        boolean isFilterOn = Utilities.getPrefBoolean(
+                mContext,
+                Constants.Preferences.PREF_FILTER_TOGGLE_KEY
+        );
+
+        RealmResults<Lesson> data;
+        if (isFilterOn) {
+            data = Utilities.Realm.getFilteredLessonsQuery(mRealm, mContext, mDay)
+                    .findAll();
+        } else {
+            data = mRealm.where(Lesson.class)
+                    .equalTo("owners.day", mDay)
+                    .findAll();
+        }
+
+        data.sort("owners.day", Sort.ASCENDING);
+
+        mRealmHelper.switchData(data);
     }
 
     @Override
     public void onDestroy() {
-
+        mRealm.close();
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return mRealmHelper.getCount();
     }
 
     @Override
@@ -42,11 +83,36 @@ public class ScheduleRemoteViewsFactory implements RemoteViewsService.RemoteView
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public boolean hasStableIds() {
-        return false;
+        return true;
+    }
+
+    private class RealmWidgetHelper {
+
+        private List<Lesson> mData;
+        private boolean mIsDataValid;
+
+        RealmWidgetHelper(List<Lesson> data) {
+            switchData(data);
+        }
+
+        public void switchData(List<Lesson> data) {
+            mData = data;
+            if (mData == null) mIsDataValid = false;
+        }
+
+        public Lesson getLesson(int position) {
+            if (!mIsDataValid) return null;
+            return mData.get(position);
+        }
+
+        public int getCount() {
+            if (!mIsDataValid) return 0;
+            return mData.size();
+        }
     }
 }
