fd57e92092beb37f8ee346e89e18c369e3dcbe9c
dodobentov@gmail.com <dodobentov@gmail.com>
2018-02-21 21:26:21 +0200

    Remove Glide

--- app/src/main/java/com/blackcracks/blich/BlichGlideModule.java
+++
@@ -1,7 +0,0 @@
-package com.blackcracks.blich;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public final class BlichGlideModule extends AppGlideModule {}
